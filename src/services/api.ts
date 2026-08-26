94d6d8d772fab0df3fb4720492f289881931efeb
Jagdish Patil <jagdishpatil5087@gmail.com>
2026-08-26 18:08:45 +0530

    check .env is working

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import { create, type Axios } from "axios";
-import { BASE_URL } from "./env";
 
 export const api: Axios = create({
-  baseURL: BASE_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
