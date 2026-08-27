a97c456993627dbe77a3ed4865b02420ff09e756
Jagdish Patil <jagdishpatil5087@gmail.com>
2026-08-27 19:01:11 +0530

    add preview.yml #1

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,69 +1,55 @@
-import { Text, View } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  Redirect,
+  Slot,
+  ThemeProvider,
+  useSegments,
+} from "expo-router";
 
-console.log("ROOT LAYOUT CONSOLE TEST");
+import { useAuthStore } from "@/stores/auth.store";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+// import { useAuthStore } from '@/stores/auth.store';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  console.log("ROOT LAYOUT RENDER");
+  const colorScheme = useColorScheme();
+  const segments = useSegments();
+
+  const { user } = useAuthStore();
+
+  const isLoading = false;
+  const initializeAuth = () => {};
+
+  useEffect(() => {
+    initializeAuth();
+  }, [initializeAuth]);
+
+  if (isLoading) {
+    return <AnimatedSplashOverlay />;
+  }
+
+  const inAuthGroup = segments[0] === "(auth)";
+
+  console.log("this console test ");
 
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Architecture Lab - DevTools Test</Text>
-    </View>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      {!user && !inAuthGroup ? (
+        <Redirect href="/(auth)/login" />
+      ) : user && inAuthGroup ? (
+        <Redirect href="/(tabs)" />
+      ) : (
+        <>
+          <AnimatedSplashOverlay />
+          <Slot />
+        </>
+      )}
+    </ThemeProvider>
   );
 }
-
-// import {
-//   DarkTheme,
-//   DefaultTheme,
-//   Redirect,
-//   Slot,
-//   ThemeProvider,
-//   useSegments,
-// } from "expo-router";
-
-// import { useAuthStore } from "@/stores/auth.store";
-// import * as SplashScreen from "expo-splash-screen";
-// import { useEffect } from "react";
-// import { useColorScheme } from "react-native";
-
-// import { AnimatedSplashOverlay } from "@/components/animated-icon";
-// // import { useAuthStore } from '@/stores/auth.store';
-
-// SplashScreen.preventAutoHideAsync();
-
-// export default function RootLayout() {
-//   const colorScheme = useColorScheme();
-//   const segments = useSegments();
-
-//   const { user } = useAuthStore();
-
-//   const isLoading = false;
-//   const initializeAuth = () => {};
-
-//   useEffect(() => {
-//     initializeAuth();
-//   }, [initializeAuth]);
-
-//   if (isLoading) {
-//     return <AnimatedSplashOverlay />;
-//   }
-
-//   const inAuthGroup = segments[0] === "(auth)";
-
-//   console.log("this console test ");
-
-//   return (
-//     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-//       {!user && !inAuthGroup ? (
-//         <Redirect href="/(auth)/login" />
-//       ) : user && inAuthGroup ? (
-//         <Redirect href="/(tabs)" />
-//       ) : (
-//         <>
-//           <AnimatedSplashOverlay />
-//           <Slot />
-//         </>
-//       )}
-//     </ThemeProvider>
-//   );
-// }
